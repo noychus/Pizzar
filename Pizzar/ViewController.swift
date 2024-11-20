eaaa6330da716846c4bbd2da46b3563989ad328b
Andrey Karetnikov <157799428+noychus@users.noreply.github.com>
2024-11-20 11:43:25 +0300

    Initial Commit

---
+++ Pizzar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pizzar
+//
+//  Created by noychus on 20.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
